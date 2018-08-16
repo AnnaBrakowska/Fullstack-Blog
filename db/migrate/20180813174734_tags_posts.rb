e51960ebcae06b33bbc1a31de984f912de9313bd
Anna <anna.brakowska91@gmail.com>
2018-08-16 14:06:01 -0400

    Added functionality for adding tags from checkbox to database

--- db/migrate/20180813174734_tags_posts.rb
+++ db/migrate/20180813174734_tags_posts.rb
@@ -1,6 +1,6 @@
 class TagsPosts < ActiveRecord::Migration[5.2]
   def change
-    create_table :posts_tags do |t| 
+    create_table :post_tags do |t| 
       t.integer :post_id
       t.integer :tag_id
     end
